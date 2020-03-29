85f4e2da825f0f2af27d5549f6d6597065d8ce49
Eduardo Castillo Fong <EDUARDCF90@GMAIL.COM>
2020-03-29 15:56:18 -0400

    Update March 29, 2020
    buildFormula function completed

--- src/App.js
+++ src/App.js
@@ -5,49 +5,79 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state =
-      {
-        formula: "123+45.2",
-        result: "456",
-        keyTriggered: " "
-      }
-      this.Write = this.Write.bind(this);
+    {
+      formula: "123+45.2",
+      result: "456",
+      keyTriggered: " "
+    }
+    this.Write = this.Write.bind(this);
   }
-  Write(event){
+  Write(event) {
     this.setState({
-      formula: this.buildFormula(event.target.value)
+      formula: this.BuildFormula(event.target.value)
     });
   }
-  buildFormula(key)
+  BuildFormula(key)  //Keys possibilities: 0 1 2 3 4 5 6 7 8 9 . + - * / = back clear
   {
-    if(key=="equals")
-    {
-      return "time to calculate";
+    var formula = this.state.formula;
+    let regexNumbers = /[0-9]/;
+    var lastKey = formula[formula.length - 1];
+    var beforelastKey = formula[formula.length - 2];
+    if (formula == '0') {
+      formula = formula + '.';
     }
-    else
+    if (regexNumbers.test(key)) {             //Test if Key is a number      
+      return formula + key;
+    }
+    if (key == "clear") {                     //Clear the display
+      return "";
+    }
+    if (key == "back")        //Clear last key
     {
-      var formula = this.state.formula;
-      let regexNumbers = /[0-9]/;
-      if((regexNumbers.test(key))||(key=="-"))
-      {
-        formula = formula + key;
-      }
-      else
-      {
-        if(key="clear")
-        {
-          return " ";
-        }
-        else
-        {
-          if((formula[formula.length-1] == "+")||(formula[formula.length-1] == "-")||(formula[formula.length-1] == "*")||(formula[formula.length-1] == "/"))
-          {
-            formula = formula.substring(0, formula.length - 1);
-          }
-            formula = formula + key;
-        }
-      }
+      formula = this.EraseLastKey(formula);
       return formula;
     }
+    if (key == "-") {
+      if ((lastKey == '+') || (lastKey == 'x') || (lastKey == '/') || (lastKey == '-')) {
+        if ((beforelastKey == '+') || (beforelastKey == 'x') || (beforelastKey == '/') || (beforelastKey == '-') || (beforelastKey == '.')) {
+          formula = this.EraseLastKey(formula);
+        }
+      }
+      return formula + key;
+    }
+    if ((key == '+') || (key == 'x') || (key == '/')) {    //If key is a symbol
+      if (formula == "") {
+        return formula;
+      }
+      if ((lastKey == '+') || (lastKey == 'x') || (lastKey == '/') || (lastKey == '.')) {
+        //If the last key was a symbol, replace it for the new symbol
+        formula = this.EraseLastKey(formula);
+      }
+      if (lastKey == '-') {
+        return formula;
+      }
+      return formula + key;
+    }
+    if (key == '.')    //If key is a decimal point (.)
+    {
+      if (!regexNumbers.test(formula[formula.length - 1])) {    //If the first key in a number is a point put cero before the (.), example 0.123
+        formula = formula + '0';
+      }
+      for (var i = formula.length - 1; i >= 0; i--) {  //Only one (.) per number
+        if ((formula[i] == '+') || (formula[i] == '-') || (formula[i] == 'x') || (formula[i] == '/')) {
+          break;
+        }
+        if (formula[i] == '.') {
+          return formula;
+        }
+      }
+      return formula + key;
+    }
+    return formula;
+  }
+  EraseLastKey(formula) {
+    var result = formula.substring(0, formula.length - 1);
+    return result;
   }
 
   render() {
@@ -55,19 +85,19 @@ class App extends React.Component {
       <div id="Calculator">
         <Display
           formula={this.state.formula}
-          result={this.state.result}/>
-        <Keyboard 
-          write={this.Write}/>
+          result={this.state.result} />
+        <Keyboard
+          write={this.Write} />
       </div>
     )
   }
 }
 
 class Display extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
   }
-  render(){
+  render() {
     return (
       <div id="display">
         <div id="displayFormula">
@@ -81,39 +111,39 @@ class Display extends React.Component {
   }
 }
 
-function Keyboard(props){
-    return (
-      <div id="mainKeyboard">
-        <KeyboardNumbers write={props.write}/>
-        <KeyboardOperators write={props.write}/>
-        <KeyboardClearEquals write={props.write}/>
-      </div>
-    )
+function Keyboard(props) {
+  return (
+    <div id="mainKeyboard">
+      <KeyboardNumbers write={props.write} />
+      <KeyboardOperators write={props.write} />
+      <KeyboardClearEquals write={props.write} />
+    </div>
+  )
 }
-function KeyboardNumbers(props){
-    return (
-      <div id="keyboardNumbers">
-        <button id="zero" className="numberButton" value={"0"} onClick={props.write}>0</button>
-        <button id="one" className="numberButton" value={"1"} onClick={props.write}>1</button>
-        <button id="two" className="numberButton" value={"2"} onClick={props.write}>2</button>
-        <button id="three" className="numberButton" value={"3"} onClick={props.write}>3</button>
-        <button id="four" className="numberButton" value={"4"} onClick={props.write}>4</button>
-        <button id="five" className="numberButton" value={"5"} onClick={props.write}>5</button>
-        <button id="six" className="numberButton" value={"6"} onClick={props.write}>6</button>
-        <button id="seven" className="numberButton" value={"7"} onClick={props.write}>7</button>
-        <button id="eight" className="numberButton" value={"8"} onClick={props.write}>8</button>
-        <button id="nine" className="numberButton" value={"9"} onClick={props.write}>9</button>
-        <button id="decimal" className="numberButton" value={"."} onClick={props.write}>.</button>
-        <button id="back" className="numberButton" value={"back"} onClick={props.write}><i class="fa fa-caret-square-o-left" aria-hidden="true"></i></button>
-      </div>
-    )
+function KeyboardNumbers(props) {
+  return (
+    <div id="keyboardNumbers">
+      <button id="zero" className="numberButton" value={"0"} onClick={props.write}>0</button>
+      <button id="one" className="numberButton" value={"1"} onClick={props.write}>1</button>
+      <button id="two" className="numberButton" value={"2"} onClick={props.write}>2</button>
+      <button id="three" className="numberButton" value={"3"} onClick={props.write}>3</button>
+      <button id="four" className="numberButton" value={"4"} onClick={props.write}>4</button>
+      <button id="five" className="numberButton" value={"5"} onClick={props.write}>5</button>
+      <button id="six" className="numberButton" value={"6"} onClick={props.write}>6</button>
+      <button id="seven" className="numberButton" value={"7"} onClick={props.write}>7</button>
+      <button id="eight" className="numberButton" value={"8"} onClick={props.write}>8</button>
+      <button id="nine" className="numberButton" value={"9"} onClick={props.write}>9</button>
+      <button id="decimal" className="numberButton" value={"."} onClick={props.write}>.</button>
+      <button id="back" className="numberButton" value={"back"} onClick={props.write}><i class="fa fa-caret-square-o-left" aria-hidden="true"></i></button>
+    </div>
+  )
 }
 function KeyboardOperators(props) {
   return (
     <div id="keyboardOperators">
       <button id="add" className="operatorButton" value={"+"} onClick={props.write}><i class="fa fa-plus" aria-hidden="true"></i></button>
       <button id="subtract" className="operatorButton" value={"-"} onClick={props.write}><i class="fa fa-minus" aria-hidden="true"></i></button>
-      <button id="multiply" className="operatorButton" value={"*"} onClick={props.write}><i class="fa fa-times" aria-hidden="true"></i></button>
+      <button id="multiply" className="operatorButton" value={"x"} onClick={props.write}><i class="fa fa-times" aria-hidden="true"></i></button>
       <button id="divide" className="operatorButton" value={"/"} onClick={props.write}><strong>/</strong></button>
     </div>
   )
@@ -122,7 +152,7 @@ function KeyboardClearEquals(props) {
   return (
     <div id="keyboardClearEquals">
       <button id="clear" value={"clear"} onClick={props.write}>AC</button>
-      <button id="equals" value={"equals"} onClick={props.write}>=</button>
+      <button id="equals" value={"="} onClick={props.write}>=</button>
     </div>
   )
 }
